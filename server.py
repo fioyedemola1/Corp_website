1de77489294743ec0024f41f3141fa29e598dddf
ademola <fioyedemola1@gmil.com>
2021-02-09 05:55:08 +0000

    ammended

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
-from flask import  Flask, render_template
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-  return "Hello World!"
+  return render_template('index.html', name='')
 
 
 
