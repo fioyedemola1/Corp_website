a46e36ddd1d461bee53273023323d80d05a05cdd
ademola <fioyedemola1@gmil.com>
2021-02-15 07:07:47 +0000

    added todo and base html

--- server.py
+++ server.py
@@ -1,11 +1,17 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+#
+# @app.route("/")
+# def hello():
+#     return render_template('index.html', name='')
 
-@app.route("/")
-def hello():
-  return render_template('index.html', name='')
+
+@app.route("test")
+def test():
+    return 'here i am'
+
 
 
 
@@ -13,4 +19,4 @@ def hello():
 
 
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
---
+++ todo.md
@@ -0,0 +1,8 @@
+* Create Templates to incorporate projects 
+  -> Html templates for projects    
+        * set up base
+        * extend base to project templates
+  
+*Integration with mailchimp
+  ->set up account/Registration
+  
