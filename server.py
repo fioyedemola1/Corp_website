345d27eb9e70108e4d7f2f449314c5cd7dff52db
ademola <fioyedemola1@gmil.com>
2021-02-15 11:31:38 +0000

    added todo and base html

--- server.py
+++ server.py
@@ -1,17 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-#
-# @app.route("/")
-# def hello():
-#     return render_template('index.html', name='')
+
+@app.route("/")
+def hello():
+    return render_template('index.html', name='')
 
 
-@app.route("test")
-def test():
-    return 'here i am'
+@app.route("/proj1",methods=["GET", 'POST'])
+def Proj1():
+    f = request.files['file'].read()
+    # do logic here
+    return render_template('proj1.html')
 
+
+
+@app.route("/proj2", methods=["GET", 'POST'])
+def proj2():
+    value = {'thing': 'thank you', 'okay': 'lols'}
+    if request.method == 'POST':
+        f = request.files['file'].read()
+        # do logic here
+    return render_template('proj2.html',value=value)
+
+
+
+
+@app.route("/styles")
+def styles():
+    return render_template('styles.html')
 
 
 
